389dd8a9d6fbb400015183783d2d96070150462d
realleesan <baominhkpkp@gmail.com>
2026-02-27 23:51:19 +0700

    Add Supabase with env vars

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://fwzzvdmezlceoagnomgo.supabase.co'
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ3enp2ZG1lemxjZW9hZ25vbWdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY1MTM0MDAsImV4cCI6MjAyMjA4OTQwMH0.NN5S70rY6Z6Y8Y6Y8Y6Y8Y6Y6Y6Y6Y6Y6Y6Y6Y6Y'
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ3enp2ZG1lemxjZW9hZ25vbWdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyMDk0MzYsImV4cCI6MjA4Nzc4NTQzNn0.V-dUrn8K1ZqxQN5gQY1PTJGY8Xm7dfi3t9f4vecFz-o'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
